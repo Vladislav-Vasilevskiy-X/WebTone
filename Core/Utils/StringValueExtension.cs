5e4653cfa86bd56c272e0176cdf1d37ecf1cc754
VladislavParkhutich <bakericecream@gmail.com>
2016-11-14 16:47:53 +0300

    Removed copyrights.

--- Core/Utils/StringValueExtension.cs
+++ Core/Utils/StringValueExtension.cs
@@ -1,5 +1,4 @@
-﻿// Copyright 2016: Thomson Reuters. All Rights Reserved. Proprietary and Confidential information of Thomson Reuters. Disclosure, Use or Reproduction without the written authorization of Thomson Reuters is prohibited.
-
+﻿
 using System;
 using System.Linq;
 
