295cdd34e4a372dc6f075f32b7f9267b3d35c6f3
VladislavParkhutich <bakericecream@gmail.com>
2016-11-15 09:47:59 +0300

    Include several files to source control.

---
+++ Core/ConfigXmlModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+
+namespace Core
+{
+	[XmlRoot("Config")]
+	public class ConfigXmlModel
+	{
+		/// <summary>
+		/// Gets or sets the browser.
+		/// </summary>
+		/// <value>The browser.</value>
+		[XmlElement("Browser")]
+		public string Browser { get; set; }
+	}
+}
